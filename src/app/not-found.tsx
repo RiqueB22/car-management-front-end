490b75fcd827f1be18fa1f0d85dd995ced1cfcf9
RiqueB22 <henrique@cnia.com.br>
2025-12-16 09:22:41 -0300

    comentando pagina not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,11 +10,17 @@ export default function NotFound() {
   const { data: session} = useSession();
 
   return (
+    // Container da pagina not found
     <div className={styles.container}>
+      {/*Card*/}
       <main className={styles.main}>
+        {/*Titulo*/}
         <h2 className={styles.title}>Pagina não encontrada</h2>
+        {/*Descrição*/}
         <p className={styles.description}>Não pode ser encontrado a pagina desejada</p>
+        {/*Status*/}
         <p className={styles.status}>Status: 404</p>
+        {/*Botão retorna para home se não estiver logado ou para tabela se estiver logado*/}
         {session ? (
           <Button className={styles.buttonHome}><Link href="/cars">Retornar</Link></Button>
         ) : (
