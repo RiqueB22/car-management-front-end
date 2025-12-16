297fdf780276747064c7ecb222a09775175cc804
RiqueB22 <henrique@cnia.com.br>
2025-12-16 09:57:29 -0300

    Comentando

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,10 @@
 import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
+// Adicionando token no next-auth
 declare module "next-auth" {
   interface Session {
     user: {
-      /** Inclui o token JWT retornado pelo backend */
+      // Inclui o token JWT retornado pelo backend
       token: string;
     } & DefaultSession["user"];
   }
